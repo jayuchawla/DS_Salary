451ab88c7d640eb710e860b6db4834004a26db06
jayuchawla78 <jayprakashchawla78@gmail.com>
2022-01-21 22:37:06 +0530

    latest changes

--- interactive_lr/interactive_lr.js
+++ interactive_lr/interactive_lr.js
@@ -1,5 +1,6 @@
 var body = document.getElementsByTagName('body')[0];
 var data = [];
+var b0=0, b1=1;
 var regressionLineContext = null;
 
 function setup() {
@@ -26,6 +27,49 @@ function markPoint(event, canvas) {
     drawCoordinatesAndRegressionLine(canvas);
 }
 
+function drawCoordinatesAndRegressionLine(canvas) {
+    b0=0;
+    b1=0;
+    if(regressionLineContext) {
+        regressionLineContext.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    for(var i = 0; i<data.length; i++) {
+        var context = canvas.getContext('2d');
+        // console.log(context);
+        context.fillStyle = '#ffffff';
+        context.beginPath();
+        context.arc(data[i][0], data[i][1], 3, 0, Math.PI*2, true);
+        context.fill();    
+    }
+    if(data.length > 1) {
+        // linearRegression();
+        gradientDescent();
+        // console.log(params);
+        regressionLineContext = drawRegressionLine(canvas);    
+    }
+    return;
+}
+
+// Why Gradient Descent ? : The formula for b1 and b0 looks very simple, even computationally, because it only works for **univariate case**, i.e. when you have only one variable. In the multivariate case, when you have many variables, the formulae is slightly more complicated on paper and requires much more calculations when you implement it in software
+// gradientDescent starts with assuming values for slopes and constant and then tweak these values based on errors in actual and predicted (using guess)
+function gradientDescent() {
+    var learningRate = 0.02;
+    for(var i = 0; i<data.length; i++) {
+        var x = data[i][0];
+        var y = data[i][1];
+        
+        var guess = b1*x + b0;
+        console.log("y: ", y, "guess: ", guess);
+        // why not square error ? cuz we are required to tune b1 and b0 according to positive and negative errors
+        var error = y - guess;
+        // console.log(error);
+        b1 = b1 + error*(x/canvas.width)*learningRate;
+        b0 = b0 + error*learningRate;
+    }
+}
+
+// b1 = sum((xi-xmean)*(yi-ymean)) / sum((xi-xmean)*(xi-xmean))
+// b0 = ymean - b1*xmean
 function linearRegression() {
     var xsum = 0;
     var ysum = 0;
@@ -43,31 +87,11 @@ function linearRegression() {
         denominator += ((data[i][0] - xmean) * (data[i][0] - xmean));
     }
     
-    var b1 = numerator / denominator;
-    var b0 = ymean - b1*xmean;
-    return [b0,b1];
+    b1 = numerator / denominator;
+    b0 = ymean - b1*xmean;
 }
 
-function drawCoordinatesAndRegressionLine(canvas) {
-    if(regressionLineContext) {
-        regressionLineContext.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    for(var i = 0; i<data.length; i++) {
-        var context = canvas.getContext('2d');
-        // console.log(context);
-        context.fillStyle = '#ffffff';
-        context.beginPath();
-        context.arc(data[i][0], data[i][1], 3, 0, Math.PI*2, true);
-        context.fill();    
-    }
-    if(data.length > 1) {
-        var params = linearRegression();
-        regressionLineContext = drawRegressionLine(params[0], params[1], canvas);    
-    }
-    return;
-}
-
-function drawRegressionLine(b0, b1, canvas) {
+function drawRegressionLine(canvas) {
     var x1 = 0;
     var y1 = b1*x1 + b0;
     var x2 = canvas.width;
